06e66f56ab3d3725749a33bea020c01e50ffea4a
Guilherme Pardi <guilhermepardi@gmail.com>
2023-09-27 13:58:03 -0300

    feat: ending header component

--- components/organisms/Header/view.tsx
+++ components/organisms/Header/view.tsx
@@ -18,44 +18,52 @@ export const Header: FC<HeaderProps> = () => {
         <Link href="/" className="flex items-center text-md text-white">
           <Logo className="w-[1.125rem] h-[1.115rem] mr-4" /> Linear
         </Link>
-        <nav
+        <div
           className={classNames(
-            'text-white h-[calc(100vh_-_var(--navigation-height))] md:block md:relative md:h-auto md:w-auto fixed md:top-0 top-navigation-height left-0 w-full overflow-auto bg-background md:bg-transparent',
-            hamburguerMenuIsOpen ? '' : 'hidden',
+            'transition-[visibility] md:visible',
+            hamburguerMenuIsOpen ? 'visible' : 'invisible delay-500',
           )}
         >
-          <ul
+          <nav
             className={classNames(
-              'flex flex-col md:flex-row md:items-center h-full [&_li]:ml-6 [&_li]:border-b [&_li]:border-grey-dark md:[&_li]:border-none',
-              '[&_a]:h-navigation-height [&_a]:w-full [&_a]:flex [&_a]:items-center [&_a]:text-md md:[&_a]:text-sm [&_a:hover]:text-grey [&_a]:transitions-colors',
+              'text-white md:opacity-100 transition-opacity duration-500 h-[calc(100vh_-_var(--navigation-height))] md:block md:relative md:h-auto md:w-auto fixed md:top-0 top-navigation-height left-0 w-full overflow-auto bg-background md:bg-transparent',
+              hamburguerMenuIsOpen ? 'opacity-100' : 'opacity-0',
             )}
           >
-            <li>
-              <Link href="#">Features</Link>
-            </li>
-            <li>
-              <Link href="#">Method</Link>
-            </li>
-            <li className="md:hidden lg:block">
-              <Link href="#">Costumers</Link>
-            </li>
-            <li className="md:hidden lg:block">
-              <Link href="#">Changelog</Link>
-            </li>
-            <li className="md:hidden lg:block">
-              <Link href="#">Integrations</Link>
-            </li>
-            <li>
-              <Link href="#">Price</Link>
-            </li>
-            <li>
-              <Link href="#">Contact</Link>
-            </li>
-            <li>
-              <Link href="#">Company</Link>
-            </li>
-          </ul>
-        </nav>
+            <ul
+              className={classNames(
+                'text-white flex flex-col md:flex-row md:items-center h-full [&_li]:ml-6 [&_li]:border-b [&_li]:border-grey-dark md:[&_li]:border-none',
+                'ease-in [&_a]:duration-300 [&_a]:translate-y-8 md:[&_a]:translate-y-0 [&_a]:h-navigation-height [&_a]:w-full [&_a]:flex [&_a]:items-center [&_a]:text-md md:[&_a]:text-sm [&_a:hover]:text-grey [&_a]:transition-[color,transform]',
+                hamburguerMenuIsOpen && '[&_a]:translate-y-0',
+              )}
+            >
+              <li>
+                <Link href="#">Features</Link>
+              </li>
+              <li>
+                <Link href="#">Method</Link>
+              </li>
+              <li className="md:hidden lg:block">
+                <Link href="#">Costumers</Link>
+              </li>
+              <li className="md:hidden lg:block">
+                <Link href="#">Changelog</Link>
+              </li>
+              <li className="md:hidden lg:block">
+                <Link href="#">Integrations</Link>
+              </li>
+              <li>
+                <Link href="#">Price</Link>
+              </li>
+              <li>
+                <Link href="#">Contact</Link>
+              </li>
+              <li>
+                <Link href="#">Company</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
 
         <div className="ml-auto h-full flex items-center text-white">
           <Link href="#" className="text-sm mr-6">
